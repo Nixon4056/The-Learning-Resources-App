9e3cfc2582aa810d576431eaf94a8894501bf609
Nikodem <nikodem@nicode.pl>
2022-07-09 16:04:09 +0200

    resources renders

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <ul>
+    <learning-resource 
+      v-for="res in storedResources" 
+      :key="res.id" 
+      :title="res.title"
+      :description="res.description"
+      :link="res.link"
+    ></learning-resource>
+  </ul>
+</template>
+
+<script>
+import LearningResource from './components/learning-resources/ResourceItem.vue';
+
+export default {
+  components: {
+    LearningResource
+  },
+  data() {
+    return{
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The official Vue.js documentation',
+          link: 'http://vuejs.org'
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Learn to google..',
+          link: 'http://google.pl'
+        },
+      ]
+    };
+  },
+}
+</script>
